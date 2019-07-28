138998b921b1d54c3692901df5cbc3f916435250
ZamoraXD <>
2019-07-28 17:59:17 +1200

    Added invulnerability in BumperCars

--- Minigames/Assets/Scripts/BumperCars/BombMovement.cs
+++ Minigames/Assets/Scripts/BumperCars/BombMovement.cs
@@ -20,6 +20,16 @@ public class BombMovement : MonoBehaviour
     // Update is called once per frame
     void Update() {
         speed = Random.Range(1f, 3f);
+        
+        if (p1Invulnerable) {
+            Physics2D.IgnoreLayerCollision(8, 9, true);
+            StartCoroutine(Vulnerability1(2));
+            Debug.Log("P1 Invulnerable!");
+        } else if (p2Invulnerable) {
+            Physics2D.IgnoreLayerCollision(8, 10, true);
+            StartCoroutine(Vulnerability2(2));
+            Debug.Log("P2 Invulnerable!");
+        }
     }
 
     //checking what the bomb is colliding with.
@@ -31,12 +41,16 @@ public class BombMovement : MonoBehaviour
 
         if ((collision.gameObject.tag == "Player1") && (p1Invulnerable == false)) {
             p1Invulnerable = true;
-            Destroy(gameObject);
+            //Destroy(gameObject);
+            gameObject.GetComponent<SpriteRenderer>().enabled = false;
+            gameObject.GetComponent<CircleCollider2D>().enabled = false;
             GameObject.Find("Background").GetComponent<BombSchtuff>().SpawnBomb();
             GameObject.Find("Background").GetComponent<BombSchtuff>().p1Lives--;
         } else if ((collision.gameObject.tag == "Player2")  && (p2Invulnerable == false)) {
             p2Invulnerable = true;
-            Destroy(gameObject);
+            //Destroy(gameObject);
+            gameObject.GetComponent<SpriteRenderer>().enabled = false;
+            gameObject.GetComponent<CircleCollider2D>().enabled = false;
             GameObject.Find("Background").GetComponent<BombSchtuff>().SpawnBomb();
             GameObject.Find("Background").GetComponent<BombSchtuff>().p2Lives--;
         }
@@ -48,13 +62,22 @@ public class BombMovement : MonoBehaviour
         gameObject.transform.localScale = new Vector3(0.25f, 0.25f, 1f);
         gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
         yield return new WaitForSeconds(waitTime);
-        Destroy(gameObject);
+        //Destroy(gameObject);
+        gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        gameObject.GetComponent<CircleCollider2D>().enabled = false;
         GameObject.Find("Background").GetComponent<BombSchtuff>().SpawnBomb();
     }
 
-    private IEnumerator Vulnerability(float waitTime) {
+    private IEnumerator Vulnerability1(float waitTime) {
         yield return new WaitForSeconds(waitTime);
-        GameObject.Find("Background").GetComponent<BombSchtuff>().SpawnBomb();
+        Physics2D.IgnoreLayerCollision(8, 9, false);
+        Debug.Log("P1 Vulnerable!");
+    }
+
+    private IEnumerator Vulnerability2(float waitTime) {
+        yield return new WaitForSeconds(waitTime);
+        Physics2D.IgnoreLayerCollision(8, 10, false);
+        Debug.Log("P2 Vulnerable!");
     }
 
 }
