09a952ce353116d219017f83f8fb4451e6fdd740
Aran <ac.arancampbellhawker@gmail.com>
2019-09-10 18:09:35 +1200

    Revert "Moving Code to Balloon"
    This reverts commit 9edbc9720edb9e2c63eefef1626397b8760fda1e.

--- Minigames/Assets/Scripts/Overcooked/ItemController.cs
+++ Minigames/Assets/Scripts/Overcooked/ItemController.cs
@@ -4,19 +4,20 @@ using UnityEngine;
 
 public class ItemController : PickUpParent
 {
-
-    public bool paused;
-
     public bool lastPlayer1;
     public bool lastPlayer2;
     public GameObject lastPlayerObj;
 
     public bool held = false;
     public bool filling = false;
-    public bool overfilling = false;
+    public bool overfill = false;
 
     public int pointValue;
     public GameObject balloonSprite;
+
+    public void Update() {
+
+    }
 
     public void LastHeldBy (GameObject other) {
       if (other.gameObject.name == "Player1") {
@@ -31,150 +32,4 @@ public class ItemController : PickUpParent
         }
 
     }
-
-    public bool dInRange;
-    public bool dispensing;
-    public int dispenseTime;
-
-    void OnTriggerEnter2D(Collider2D other) {
-      if (other.tag == "Dispenser") {
-        dispenseTime = other.gameObject.GetComponent<Dispenser>().dispenseTime;
-        dInRange = true;
-      }
-      while (dInRange) {
-        if (dispensing == false && held == false) {
-          dispensing = true;
-          filling = true;
-          StartCoroutine(FillingBalloon(other.gameObject));
-        }
-      }
-    }
-
-    void Update() {
-
-    }
-
-    //handles "filling" the balloon
-    IEnumerator FillingBalloon (GameObject other) {
-        //tempLastHeld = lastPlayerObj;
-        //Destroy(other.gameObject);
-        float l = 0;
-        while (l < dispenseTime) {
-            if (paused) {
-                yield return null;
-            } else {
-                l += Time.deltaTime;
-              //  Vector4.Lerp(Vector4 other  , Vector4  , (l/4));
-                yield return null;
-            }
-        }
-        //other = Instantiate(dispensedB, this.gameObject.transform.position, Quaternion.identity);
-        overfilling = true;
-        //other.gameObject.GetComponent<ItemController>().lastPlayerObj = tempLastHeld;
-        StartCoroutine(Overfill());
-    }
-
-    //checks if the balloon is being overfilled, and then makes it explode
-    IEnumerator Overfill () {
-        float l = 0;
-        while (l < 2.5f) {
-            if (paused) {
-                yield return null;
-            } else {
-                l += Time.deltaTime;
-                yield return null;
-            }
-        }
-        if (overfilling) {
-            Destroy(gameObject);
-            dispensing = false;
-        }
-    }
-
-    void OnTriggerExit2D(Collider2D other) {
-        if (gameObject.tag == "PickUp" && dispensing == true) {
-            dispensing = false;
-        }
-    }
-    /*public GameObject dispensedB;
-    public int dispenseTime;
-    public bool dispensing;
-    public bool dispensingP2;
-
-    private GameObject tempLastHeld;
-
-    public Color dColor;
-    public bool paused;
-
-    //calling colour change spritesheet.
-    void OnTriggerStay2D(Collider2D other) {
-        if (other.gameObject.name == "DrinkEmpty(Clone)" && dispensingP1 == false) {
-            if (other.gameObject.GetComponent<ItemController>().held == false && other.gameObject.GetComponent<ItemController>().lastPlayer1 == true) {
-                dispensingP1 = true;
-                filling = true;
-                StartCoroutine(FillingBalloon(other.gameObject, true, false));
-            }
-      }
-        if (other.gameObject.name == "DrinkEmpty(Clone)" && dispensingP2 == false)
-        {
-            if (other.gameObject.GetComponent<ItemController>().held == false && other.gameObject.GetComponent<ItemController>().lastPlayer2 == true)
-            {
-                dispensingP2 = true;
-                other.gameObject.GetComponent<ItemController>().filling = true;
-                StartCoroutine(FillingBalloon(other.gameObject, false, true));
-            }
-        }
-    }
-
-    //handles "filling" the balloon
-    IEnumerator FillingBalloon (GameObject other, bool p1, bool p2) {
-        tempLastHeld = other.gameObject.GetComponent<ItemController>().lastPlayerObj;
-        Destroy(other.gameObject);
-        float l = 0;
-        while (l < dispenseTime) {
-            if (paused) {
-                yield return null;
-            } else {
-                l += Time.deltaTime;
-                yield return null;
-            }
-        }
-        other = Instantiate(dispensedB, this.gameObject.transform.position, Quaternion.identity);
-        other.gameObject.GetComponent<ItemController>().overfill = true;
-        other.gameObject.GetComponent<ItemController>().lastPlayerObj = tempLastHeld;
-        StartCoroutine(Overfill(other.gameObject, p1, p2));
-    }
-
-    //checks if the balloon is being overfilled, and then makes it explode
-    IEnumerator Overfill (GameObject other, bool p1, bool p2) {
-        float l = 0;
-        while (l < 2.5f) {
-            if (paused) {
-                yield return null;
-            } else {
-                l += Time.deltaTime;
-                yield return null;
-            }
-        }
-        if (other.gameObject != null && other.gameObject.GetComponent<ItemController>().overfill) {
-            Destroy(other.gameObject);
-            if (p1) {
-                dispensingP1 = false;
-            } else {
-                dispensingP2 = false;
-            }
-        } else {
-            other = null;
-        }
-    }
-
-    void OnTriggerExit2D(Collider2D other) {
-        if (other.gameObject.tag == "PickUp" && other.gameObject.name != "DrinkEmpty(Clone)" && dispensingP1 == true) {
-            dispensingP1 = false;
-        }
-        if (other.gameObject.tag == "PickUp" && other.gameObject.name != "DrinkEmpty(Clone)" && dispensingP2 == true)
-        {
-            dispensingP2 = false;
-        }
-    } */
 }
