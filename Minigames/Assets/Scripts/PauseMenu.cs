448e8a71043ae18c8c37a976f00d552fb305715b
tfihsnz <ljburrough@gmail.com>
2019-09-12 16:49:42 +1200

    updated the mute button to change to unmute and back
    luke 12/09/2019 lukebranch2

--- Minigames/Assets/Scripts/PauseMenu.cs
+++ Minigames/Assets/Scripts/PauseMenu.cs
@@ -8,79 +8,75 @@ public class PauseMenu : MonoBehaviour
     public GameObject PausedMenu;
     public GameObject PauseManagerRef;
     public GameObject AreYouSure;
+    public GameObject mutebutton;
+    public GameObject unmutebutton;
     public bool Paused;
     private string CurrentScene;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         Scene scene = SceneManager.GetActiveScene();
-        if (scene.name == "BumperCarsMG")
-        {
+        if (scene.name == "BumperCarsMG") {
             CurrentScene = "BumperCars";
         }
-        else if (scene.name == "OvercookedMG")
-        {
+        else if (scene.name == "OvercookedMG") {
             CurrentScene = "OverCooked";
         }
-        else if (scene.name == "BulletHell")
-        {
+        else if (scene.name == "BulletHell") {
             CurrentScene = "BulletHell";
         }
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
 
     }
 
-    public void togglePauseMenu()
-    {
-        if (Paused == false)
-        {
+    public void togglePauseMenu() {
+        if (Paused == false) {
             PausedMenu.SetActive(true);
             Paused = true;
         }
-        else if (Paused == true)
-        {
+        else if (Paused == true) {
             PausedMenu.SetActive(false);
             Paused = false;
         }
 
     }
 
-    public void BackToMainCheck()
-    {
+    public void BackToMainCheck() {
         AreYouSure.SetActive(true);
     }
 
-    public void CloseAlert()
-    {
+    public void CloseAlert() {
         AreYouSure.SetActive(false);
     }
 
-    public void BackToMenu()
-    {
+    public void BackToMenu() {
         AreYouSure.SetActive(false);
         SceneManager.LoadScene("MainMenu");
-
     }
 
-    public void UnPause()
-    {
-        if (CurrentScene == "BumperCars")
-        {
+    public void MuteSound() {
+        mutebutton.SetActive(false);
+        unmutebutton.SetActive(true);
+    }
+
+    public void unMuteSound() {
+        unmutebutton.SetActive(false);
+        mutebutton.SetActive(true);
+    }
+
+    public void UnPause() {
+        if (CurrentScene == "BumperCars") {
             PauseManagerRef.GetComponent<Pause_BumperCars>().PauseButton();
             togglePauseMenu();
         }
-        else if (CurrentScene == "OverCooked")
-        {
+        else if (CurrentScene == "OverCooked") {
             PauseManagerRef.GetComponent<Pause_Overcooked>().PauseButton();
             togglePauseMenu();
         }
-        else if (CurrentScene == "BulletHell")
-        {
+        else if (CurrentScene == "BulletHell") {
             PauseManagerRef.GetComponent<Pause_BulletHell>().PauseButton();
             togglePauseMenu();
         }
