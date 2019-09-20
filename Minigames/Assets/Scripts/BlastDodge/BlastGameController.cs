84cee7fcad213b80076a41154ebd8a6174ec6408
Aran <ac.arancampbellhawker@gmail.com>
2019-09-20 23:21:58 +1200

    Blast Dodge Base SetUp
    Blast Dodge Added to base game
    
    Aran
    11:21pm 20/09/2019

---
+++ Minigames/Assets/Scripts/BlastDodge/BlastGameController.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlastGameController : MonoBehaviour
+{
+    /*
+        Dictionary of N horizontal Points = 16
+        Dictionary of S horizontal Points = 16
+        Dictionary of E horizontal Points = 12
+        Dictionary of W horizontal Points = 12
+
+        NW -4.4, 3
+        SE 4.4, -3
+    */
+
+    public GameObject pickUp;
+    private Vector2 nw = new Vector2(-4.4f, 3f);
+    private Vector2 se = new Vector2(4.4f, -3f);
+    private float timer;
+    private float spawnTime = 5;
+
+    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+    //Blast object list setting
+    public GameObject northCones;
+    public GameObject southCones;
+    public GameObject northLines;
+    public GameObject southLines;
+    public GameObject eastLines;
+    public GameObject westLines;
+
+    List <GameObject> spawnsNorthCones = new List <GameObject>(); //index total of 9
+    List <GameObject> spawnsSouthCones = new List <GameObject>(); //index total of 9
+    List <GameObject> spawnsNorthLines = new List <GameObject>(); //index total of 9
+    List <GameObject> spawnsSouthLines = new List <GameObject>(); //index total of 9
+    List <GameObject> spawnsEastLines = new List <GameObject>(); //index total of 7
+    List <GameObject> spawnsWestLines = new List <GameObject>(); //index total of 7
+
+
+    void Start() {
+        foreach (Transform transform in northCones.transform) {
+            spawnsNorthCones.Add(transform.gameObject);
+        }
+        foreach (Transform transform in southCones.transform) {
+            spawnsSouthCones.Add(transform.gameObject);
+        }
+        foreach (Transform transform in northLines.transform) {
+            spawnsNorthLines.Add(transform.gameObject);
+        }
+        foreach (Transform transform in southLines.transform) {
+            spawnsSouthLines.Add(transform.gameObject);
+        }
+        foreach (Transform transform in eastLines.transform) {
+            spawnsEastLines.Add(transform.gameObject);
+        }
+        foreach (Transform transform in westLines.transform) {
+            spawnsWestLines.Add(transform.gameObject);
+        }
+        PickUpSpawn();
+    }
+
+    void Update() {
+        timer += Time.deltaTime;
+        if (timer >= spawnTime) {
+            PickUpSpawn();
+            BlastEm();
+            timer = 0;
+        }
+    }
+
+    void PickUpSpawn() {
+        Instantiate(pickUp, new Vector3(Random.Range(nw.x, se.x), Random.Range(nw.y, se.y), -5), Quaternion.identity);
+    }
+
+    void BlastEm() {
+        int randomInt;
+        int randomNSInt;
+        int randomEWInt;
+        randomInt = Random.Range(0,4);
+        randomNSInt = Random.Range(0,9);
+        if (randomInt == 0) {
+            //North
+            //spawnsNorthLines[randomNSInt].SetActive(true);
+            spawnsNorthCones[randomNSInt].SetActive(true);
+        } else if (randomInt == 1) {
+            //East
+            //spawnsSouthLines[randomNSInt].SetActive(true);
+            spawnsSouthCones[randomNSInt].SetActive(true);
+        } else if (randomInt == 2) {
+            //South
+            spawnsSouthCones[randomNSInt].SetActive(true);
+        } else {
+            //West
+            spawnsNorthCones[randomNSInt].SetActive(true);
+        }
+    }
+}
---
+++ Minigames/Assets/Scripts/PC_BlastDodge.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PC_BlastDodge : PlayerController
+{
+
+    private int points;
+
+    private float timer;
+    private float stunTime = 1f;
+    private bool stunned;
+    public GameObject currency;
+
+    // Start is called before the first frame update
+    protected override void Start() {
+        base.Start();
+        speed = 7f;
+        rotationSpeed = 100.0f;
+    }
+
+    // Update is called once per frame
+    protected override void Update() {
+        base.Update();
+        if (stunned) {
+            speed = 0f;
+            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+            timer += Time.deltaTime;
+            if (timer >= stunTime) {
+                stunned = false;
+                speed = 7f;
+                timer = 0;
+            }
+        }
+    }
+
+    protected override void FixedUpdate() {
+        base.FixedUpdate();
+
+        //Player forward/backward movement and rotation.
+        if (paused) {
+            return;
+        } else {
+            if (gameObject.tag == "Player1") {
+                GetComponent<Rigidbody2D>().AddForce(transform.up * Input.GetAxis("VerticalP1") * speed);
+                transform.Rotate(0f, 0f, Input.GetAxis("HorizontalP1") * rotationSpeed * Time.deltaTime * -1);
+            } else if (gameObject.tag == "Player2") {
+                GetComponent<Rigidbody2D>().AddForce(transform.up * Input.GetAxis("VerticalP2") * speed);
+                transform.Rotate(0f, 0f, Input.GetAxis("HorizontalP2") * rotationSpeed * Time.deltaTime * -1);
+            } else {
+                return;
+            }
+        }
+    }
+
+    public void HandlePoints(int tempPoints, GameObject tempGO) {
+        if (tempGO.tag == "PickUp") {
+            points += tempPoints;
+            Debug.Log("Points after pick up : " + points);
+        }
+        if (tempGO.tag == "Blast") {
+            points -= tempPoints;
+            if (points <= -1) {
+                points = 0;
+            }
+            Debug.Log("Points after hit : " + points);
+        }
+    }
+
+    public void HandleHit(int spawnInt) {
+        //Stun Player
+        stunned = true;
+
+        //Drop Currency
+        if (points >= 1) {
+            for (int i = 0; i <= spawnInt; i++) {
+                if (i >= spawnInt +1) {
+                    break;
+                }
+                int randomInt;
+                randomInt = Random.Range(0, 4);
+                if (randomInt == 0) {
+                    //North
+                } else if (randomInt == 1) {
+                    //East
+                } else if (randomInt == 2) {
+                    //South
+                } else {
+                    //West
+                }
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag == "PickUp") {
+            HandlePoints(1, other.gameObject);
+            Destroy(other.gameObject);
+        }
+        // if (other.gameObject.tag == "Blast") {
+        //     HandlePoints(4, other.gameObject);
+        //     HandleHit(4);
+        // }
+    }
+    void OnParticleCollision(GameObject other) {
+        Debug.Log(other);
+        //HandlePoints(4, other.gameObject);
+        HandleHit(4);
+    }
+}
