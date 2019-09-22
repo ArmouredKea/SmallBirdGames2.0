826794c5eca36ebba05f67772204bde778b838c5
ZamoraXD <>
2019-09-22 22:24:50 +1200

    Joysticks Carryover
    GeneralSchtuff3 - 22/09/19

--- Minigames/Assets/Scripts/CharacterManagement.cs
+++ Minigames/Assets/Scripts/CharacterManagement.cs
@@ -12,24 +12,40 @@ public class CharacterManagement : MonoBehaviour
     public GameObject p2Bo;
     public GameObject p2Hiro;
     public GameObject p2Mika;
+    public GameObject p1JoystickInner;
+    public GameObject p1JoystickOuter;
+    public GameObject p2JoystickInner;
+    public GameObject p2JoystickOuter;
 
 
     // Start is called before the first frame update
     void Awake() {
         if (CharacterCarryOver.player1 == "Bo") {
             p1Bo.SetActive(true);
+            p1JoystickInner.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("JoystickInner_Bo");
+            p1JoystickOuter.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("JoystickOuter_Bo");
         } else if (CharacterCarryOver.player1 == "Hiro") {
             p1Hiro.SetActive(true);
+            p1JoystickInner.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("JoystickInner_Hiro");
+            p1JoystickOuter.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("JoystickOuter_Hiro");
         } else if (CharacterCarryOver.player1 == "Mika") {
             p1Mika.SetActive(true);
+            p1JoystickInner.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("JoystickInner_Mika");
+            p1JoystickOuter.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("JoystickOuter_Mika");
         }
 
         if (CharacterCarryOver.player2 == "Bo") {
             p2Bo.SetActive(true);
+            p2JoystickInner.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("JoystickInner_Bo");
+            p2JoystickOuter.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("JoystickOuter_Bo");
         } else if (CharacterCarryOver.player2 == "Hiro") {
             p2Hiro.SetActive(true);
+            p2JoystickInner.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("JoystickInner_Hiro");
+            p2JoystickOuter.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("JoystickOuter_Hiro");
         } else if (CharacterCarryOver.player2 == "Mika") {
             p2Mika.SetActive(true);
+            p2JoystickInner.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("JoystickInner_Mika");
+            p2JoystickOuter.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("JoystickOuter_Mika");
         }
     }
 
