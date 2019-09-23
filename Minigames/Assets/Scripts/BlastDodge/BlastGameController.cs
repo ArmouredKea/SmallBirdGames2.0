37172d07537ddc1f1a316e8fba191058e13bfc82
Aran <ac.arancampbellhawker@gmail.com>
2019-09-23 17:16:28 +1200

    new tag and prefab

--- Minigames/Assets/Scripts/BlastDodge/BlastGameController.cs
+++ Minigames/Assets/Scripts/BlastDodge/BlastGameController.cs
@@ -22,6 +22,10 @@ public class BlastGameController : MonoBehaviour
 
     public GameObject thrown;
 
+    public Queue<GameObject> balloonQueue = new Queue<GameObject>();
+    private float fireTime;
+    private bool fire;
+
     /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
     //Blast object list setting
     public GameObject northCones;
@@ -34,9 +38,10 @@ public class BlastGameController : MonoBehaviour
     List <GameObject> spawnsNorthCones = new List <GameObject>(); //index total of 9
     List <GameObject> spawnsSouthCones = new List <GameObject>(); //index total of 9
     List <GameObject> spawnsNorthLines = new List <GameObject>(); //index total of 9
-    List <GameObject> spawnsSouthLines = new List <GameObject>(); //index total of 9
+    public List <GameObject> spawnsSouthLines = new List <GameObject>(); //index total of 9
     List <GameObject> spawnsEastLines = new List <GameObject>(); //index total of 7
     List <GameObject> spawnsWestLines = new List <GameObject>(); //index total of 7
+    List <Vector3> lineTargets = new List <Vector3>();
 
 
     void Start() {
@@ -62,6 +67,8 @@ public class BlastGameController : MonoBehaviour
         for (int i = 0; i <= 55; i++) {
             tempGO = Instantiate (thrown, new Vector3(0, 0, 0), Quaternion.identity);
             tempGO.SetActive(false);
+            balloonQueue.Enqueue(tempGO);
+            Debug.Log(balloonQueue.Peek());
         }
         PickUpSpawn();
     }
@@ -86,26 +93,45 @@ public class BlastGameController : MonoBehaviour
 
     //per splat, 0.2s between each
 
+    //SouthV Sp -0.4
+    //SouthV 1st -3.45 (+1.15)
+
     void BlastEm() {
         int randomInt;
         int randomNSInt;
         int randomEWInt;
         randomInt = Random.Range(0,4);
         randomNSInt = Random.Range(0,9);
-        if (randomInt == 0) {
-            //North
-            //spawnsNorthLines[randomNSInt].SetActive(true);
-            spawnsNorthCones[randomNSInt].SetActive(true);
-        } else if (randomInt == 1) {
-            //East
-            //spawnsSouthLines[randomNSInt].SetActive(true);
-            spawnsSouthCones[randomNSInt].SetActive(true);
-        } else if (randomInt == 2) {
-            //South
-            spawnsSouthCones[randomNSInt].SetActive(true);
-        } else {
-            //West
-            spawnsNorthCones[randomNSInt].SetActive(true);
+
+        spawnsSouthLines[randomNSInt].SetActive(true);
+        foreach (Transform child in spawnsSouthLines[randomNSInt].transform) {
+            if (child.tag == "Target") {
+                lineTargets.Add(child.transform.position);
+            }
         }
+        for (int i = 0; i <= 11; i++) {
+            GameObject b;
+            b = balloonQueue.Dequeue();
+            //b.GetComponent<ThrownBalloonScript>().throwTime = (fireTime += 0.2f);
+            b.transform.position = new Vector3(spawnsSouthLines[randomNSInt].transform.position.x, (spawnsSouthLines[randomNSInt].transform.position.y -= 0.4f), spawnsSouthLines[randomNSInt].transform.position.z);
+            //b.GetComponent<ThrownBalloonScript>().target += lineTargets[i];
+            b.SetActive(true);
+        }
+
+        // if (randomInt == 0) {
+        //     //North
+        //     //spawnsNorthLines[randomNSInt].SetActive(true);
+        //     spawnsNorthCones[randomNSInt].SetActive(true);
+        // } else if (randomInt == 1) {
+        //     //East
+        //     //spawnsSouthLines[randomNSInt].SetActive(true);
+        //     spawnsSouthCones[randomNSInt].SetActive(true);
+        // } else if (randomInt == 2) {
+        //     //South
+        //     spawnsSouthCones[randomNSInt].SetActive(true);
+        // } else {
+        //     //West
+        //     spawnsNorthCones[randomNSInt].SetActive(true);
+        // }
     }
 }
---
+++ Minigames/Assets/ThrownScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrownScript : MonoBehaviour
+{
+    public GameObject gameController;
+
+    public Vector3 target;
+    public float throwTime;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameController = GameObject.FindGameObjectWithTag("GameController");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
