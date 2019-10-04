d4f4e3bb811feea6f4c91e98d69f9c45fd49d623
Aran <ac.arancampbellhawker@gmail.com>
2019-10-04 18:33:56 +1300

    Temp AudioManager Fix
    bf-AudioFix
    Aran
    04/10/2019

--- Minigames/Assets/Scripts/SceneManagement.cs
+++ Minigames/Assets/Scripts/SceneManagement.cs
@@ -12,10 +12,10 @@ public class SceneManagement : MonoBehaviour
     private string nextScene = "";
     private static List<int> scenes = new List<int>(Enumerable.Range(1, 3));
     private static int gameLength;
-
+    private static bool muted;
     // Start is called before the first frame update
     void Start() {
-
+        GameObject.FindGameObjectWithTag("AudioManager").isMuted = muted;
     }
 
     // Update is called once per frame
@@ -36,16 +36,20 @@ public class SceneManagement : MonoBehaviour
             int minigame = scenes[randomIndex] + 1;
             scenes.RemoveAt(randomIndex);
             SceneManager.LoadScene(minigame);
+            muted = GameObject.FindGameObjectWithTag("AudioManager").isMuted
         } else if (scenes.Count == 0 && gameLength == 0) {
             gameLength--;
             scenes = new List<int>(Enumerable.Range(1, 3));
             SceneManager.LoadScene("End");
+            Destroy(GameObject.FindGameObjectWithTag("AudioManager"));
+            muted = GameObject.FindGameObjectWithTag("AudioManager").isMuted
         } else {
             gameLength--;
             int randomIndex = Random.Range(0, scenes.Count);
             int minigame = scenes[randomIndex] + 1;
             scenes.RemoveAt(randomIndex);
             SceneManager.LoadScene(minigame);
+            muted = GameObject.FindGameObjectWithTag("AudioManager").isMuted
         }
     }
 
