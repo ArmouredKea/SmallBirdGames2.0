caa69e93646449bd6aa440f5acb6f91c9b4e082f
ZamoraXD <>
2019-10-17 18:19:34 +1300

    Audio Script

--- Minigames/Assets/Scripts/AudioManagerScript.cs
+++ Minigames/Assets/Scripts/AudioManagerScript.cs
@@ -3,11 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AudioManagerScript : MonoBehaviour
-{
+public class AudioManagerScript : MonoBehaviour {
 
     [Serializable]
-    public class StringAudioClipDictionary : SerializableDictionary<string, AudioClip> {}
+    public class StringAudioClipDictionary : SerializableDictionary<string, AudioClip> { }
 
     public StringAudioClipDictionary soundsDicitonary;
 
@@ -20,43 +19,45 @@ public class AudioManagerScript : MonoBehaviour
     public bool isMuted;
     // Start is called before the first frame update
     void Awake() {
-      foreach (AudioClip ac in Resources.LoadAll("", typeof(AudioClip))) {
-        string audioName = ac.name;
-        soundsDicitonary.Add(audioName, ac);
-      }
+        foreach (AudioClip ac in Resources.LoadAll("", typeof(AudioClip))) {
+            string audioName = ac.name;
+            soundsDicitonary.Add(audioName, ac);
+        }
 
-      DontDestroyOnLoad(this.gameObject);
+        DontDestroyOnLoad(this.gameObject);
     }
 
-    void Update () {
+    void Update() {
         if (isMuted && !GameObject.FindGameObjectWithTag("Music").GetComponent<AudioSource>().mute) {
+            MuteAudio();
             GameObject.FindGameObjectWithTag("Music").GetComponent<AudioSource>().mute = true;
         } else if (!isMuted && GameObject.FindGameObjectWithTag("Music").GetComponent<AudioSource>().mute) {
             GameObject.FindGameObjectWithTag("Music").GetComponent<AudioSource>().mute = false;
+            UnMuteAudio();
         }
     }
 
     public void PlayAudio(string clipName) {
 
-      AudioClip temp = null;
-      if (soundsDicitonary.TryGetValue(clipName, out temp)) {
-        if (audioSource1.isPlaying == false) {
-            audioSource1.clip = temp;
-            audioSource1.Play(0);
-        } else if (audioSource2.isPlaying == false) {
-            audioSource2.clip = temp;
-            audioSource2.Play(0);
-        } else if (audioSource3.isPlaying == false) {
-            audioSource3.clip = temp;
-            audioSource3.Play(0);
-        } else if (audioSource4.isPlaying == false) {
-            audioSource4.clip = temp;
-            audioSource4.Play(0);
-        } else if (audioSource5.isPlaying == false) {
-            audioSource5.clip = temp;
-            audioSource5.Play(0);
+        AudioClip temp = null;
+        if (soundsDicitonary.TryGetValue(clipName, out temp)) {
+            if (audioSource1.isPlaying == false) {
+                audioSource1.clip = temp;
+                audioSource1.Play(0);
+            } else if (audioSource2.isPlaying == false) {
+                audioSource2.clip = temp;
+                audioSource2.Play(0);
+            } else if (audioSource3.isPlaying == false) {
+                audioSource3.clip = temp;
+                audioSource3.Play(0);
+            } else if (audioSource4.isPlaying == false) {
+                audioSource4.clip = temp;
+                audioSource4.Play(0);
+            } else if (audioSource5.isPlaying == false) {
+                audioSource5.clip = temp;
+                audioSource5.Play(0);
+            }
         }
-      }
     }
 
     public void MuteAudio() {
@@ -65,9 +66,9 @@ public class AudioManagerScript : MonoBehaviour
         }
     }
     public void UnMuteAudio() {
-      foreach (Transform child in transform) {
-          child.GetComponent<AudioSource>().mute = false;
-      }
+        foreach (Transform child in transform) {
+            child.GetComponent<AudioSource>().mute = false;
+        }
     }
-    
+
 }
