708680a8dfee0ca8f120d2f3f1dab6e3350f9183
ZamoraXD <>
2019-10-18 19:01:25 +1300

    Tag Greybox

---
+++ Minigames/Assets/Scripts/PC_Tag.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PC_Tag : PlayerController
+{
+
+    private float timer = 8f;
+    public bool tagged = false;
+    public bool it = false;
+    public bool boosted;
+    public bool slowed;
+    public GameObject tagIcon;
+    public GameObject pointLight;    
+
+    // Start is called before the first frame update
+    protected override void Start() {
+        base.Start();
+
+        speed = 7f;
+        rotationSpeed = 100.0f;
+    }
+
+    // Update is called once per frame
+    protected override void Update() {
+        base.Update();        
+
+        if (it) {
+            tagIcon.SetActive(true);            
+        } else {
+            tagIcon.SetActive(false);
+        }
+
+        if (tagged) {
+            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        }
+    }
+
+    protected override void FixedUpdate() {
+        base.FixedUpdate();
+
+        //Player forward/backward movement and rotation.
+        if (paused) {
+            return;
+        } else {
+            if (gameObject.tag == "Player1") {
+                GetComponent<Rigidbody2D>().AddForce(transform.up * Input.GetAxis("VerticalP1") * speed);
+                transform.Rotate(0f, 0f, Input.GetAxis("HorizontalP1") * rotationSpeed * Time.deltaTime * -1);
+            } else if (gameObject.tag == "Player2") {
+                GetComponent<Rigidbody2D>().AddForce(transform.up * Input.GetAxis("VerticalP2") * speed);
+                transform.Rotate(0f, 0f, Input.GetAxis("HorizontalP2") * rotationSpeed * Time.deltaTime * -1);
+            } else {
+                return;
+            }
+        }
+    }
+
+    //move character based on joystick directions.
+    protected override void MoveCharacter(Vector2 direction) {
+        base.MoveCharacter(direction);
+        gameObject.GetComponent<Rigidbody2D>().AddForce(direction * speed * 2);
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision) {
+
+        if ((collision.gameObject.tag == "Player1" || collision.gameObject.tag == "Player2") && it == false) {
+            tagged = true;
+            it = true;
+            gameObject.GetComponent<CircleCollider2D>().enabled = false;
+            tagIcon.SetActive(true);
+            pointLight.GetComponent<Light>().spotAngle = 60;            
+            speed = 0;
+            StartCoroutine(TagDelay(2f));
+        } else if ((collision.gameObject.tag == "Player1" || collision.gameObject.tag == "Player2") && it == true) {
+            it = false;
+            tagIcon.SetActive(false);
+            pointLight.GetComponent<Light>().spotAngle = 50;
+        }
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.tag == "Powerup") {
+            Destroy(collision.gameObject);
+            float i = Random.Range(0f, 0.9f);
+            if (i < 0.5f) {
+                SpeedPowerup();
+                Debug.Log("Speed Powerup");
+            } else {
+                LightPowerup();
+                Debug.Log("Light Powerup");
+            }
+        } else if (collision.gameObject.tag == "Trap") {
+            Destroy(collision.gameObject);
+            float i = Random.Range(0f, 0.9f);
+            if (i < 0.5f) {
+                SpeedTrap();
+                Debug.Log("Speed Trap");
+            } else {
+                LightTrap();
+                Debug.Log("Light Trap");
+            }
+        }
+    }    
+
+    private IEnumerator TagDelay(float waitTime) {
+
+        yield return new WaitForSeconds(waitTime);
+        tagged = false;
+        gameObject.GetComponent<CircleCollider2D>().enabled = true;
+        speed = 7f;
+
+    }
+
+    public void SpeedPowerup() {
+        speed = 10f;
+        boosted = true;
+        StartCoroutine(SpeedBoost(4f));
+    }
+
+    public void SpeedTrap() {
+        speed = 2f;
+        slowed = true;
+        StartCoroutine(SpeedSlow(2f));
+    }
+
+    public void LightPowerup() {
+        pointLight.GetComponent<Light>().spotAngle += 10;
+    }
+
+    public void LightTrap() {
+        if (pointLight.GetComponent<Light>().spotAngle >= 10) {
+            pointLight.GetComponent<Light>().spotAngle -= 10;
+        }        
+    }
+
+    private IEnumerator SpeedBoost(float waitTime) {
+
+        yield return new WaitForSeconds(waitTime);
+        if (!slowed) {
+            speed = 7f;
+        }
+        boosted = false;
+
+    }
+
+    private IEnumerator SpeedSlow(float waitTime) {
+
+        yield return new WaitForSeconds(waitTime);
+        if (!boosted) {
+            speed = 7f;
+        }
+        slowed = false;
+
+    }
+
+}
--- Minigames/Assets/Scripts/Tag/TagGameplay.cs
+++ Minigames/Assets/Scripts/Tag/TagGameplay.cs
@@ -1,24 +1,104 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TagGameplay : MonoBehaviour
 {
+
+    public float timer = 60f;
+    public float spawnTimer = 8f;
+    public GameObject p1Light;
+    public GameObject p2Light;
+    public GameObject winText;
+    public GameObject walls1;
+    public GameObject walls2;
+    public GameObject walls3;
+    public Transform powerup;
+    public Transform trap;
+    private float j;
+    private float m;
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         float i = Random.Range(0f, 0.9f);
 
-        if (i < 0.5f) {
-            GameObject.Find("Player1").GetComponent<TagScript>().it = true;
+        if (i < 0.45f) {
+            GameObject.FindWithTag("Player1").GetComponent<PC_Tag>().it = true;
+            p1Light.GetComponent<Light>().spotAngle = 60;
         } else {
-            GameObject.Find("Player2").GetComponent<TagScript>().it = true;
+            GameObject.FindWithTag("Player2").GetComponent<PC_Tag>().it = true;
+            p2Light.GetComponent<Light>().spotAngle = 60;
+        }
+
+        if (i < 0.3f) {
+            walls1.SetActive(true);
+        } else if (i < 0.6f) {
+            walls2.SetActive(true);
+        } else if (i < 0.9f) {
+            walls3.SetActive(true);
         }
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        timer -= Time.deltaTime;
+        spawnTimer -= Time.deltaTime;
+
+        if (spawnTimer <= 0f) {
+            SpawnPickup();
+            spawnTimer = 8f;
+        }
+
+        if (timer <= 0) {
+            winText.SetActive(true);
+            if (GameObject.FindWithTag("Player1").GetComponent<PC_Tag>().it == true) {
+                winText.GetComponent<Text>().text = "Player 2 Wins!";
+            } else if (GameObject.FindWithTag("Player2").GetComponent<PC_Tag>().it == true) {
+                winText.GetComponent<Text>().text = "Player 1 Wins!";
+            }
+            Time.timeScale = 0;
+        }
     }
+
+    //spawns a pickup at a random position
+    public void SpawnPickup() {
+        float l = Random.Range(0f, 0.8f);        
+
+        if (l < 0.1f) {
+            m = -4.5f;
+            j = 3.7f;
+        } else if (l < 0.2f) {
+            m = 4.5f;
+            j = 3.7f;
+        } else if (l < 0.3f) {
+            m = -4.5f;
+            j = -3.7f;
+        } else if (l < 0.4f) {
+            m = 4.5f;
+            j = -3.7f;
+        } else if (l < 0.5f) {
+            m = 0f;
+            j = -3.7f;
+        } else if (l < 0.6f) {
+            m = 0f;
+            j = 3.7f;
+        } else if (l < 0.7f) {
+            m = -1.5f;
+            j = 0f;
+        } else if (l <= 0.8f) {
+            m = 1.5f;
+            j = 0f;
+        }
+
+        float k = Random.Range(0f, 1f);
+        if (k < 0.5f) {
+            Instantiate(powerup, new Vector2(m, j), Quaternion.identity);
+        } else if (k <= 1f) {
+            Instantiate(trap, new Vector2(m, j), Quaternion.identity);
+        }
+        
+
+    }
+
 }
